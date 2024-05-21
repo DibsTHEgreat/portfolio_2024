23fedb36b21643aa623cbd4f1c5ac9990876b048
DibsTHEgreat <pateliyandivya@gmail.com>
2024-05-21 00:54:02 -0600

    recent work

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import AboutMe from "@/components/AboutMe";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import RecentWork from "@/components/RecentWork";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col overflow-hidden mx-auto sm:px-10  px-5">
       <Navbar />
-      <div className="w-full justify-center items-center">
-          <Hero />
-          <AboutMe />
-          <Footer />
-      </div>
+        <div className="w-full justify-center items-center">
+            <Hero />
+            <AboutMe />
+            <RecentWork />
+        </div>
+      <Footer />
     </main>
   );
 }
---
+++ components/RecentWork.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecentWork = () => {
+  return (
+    <div>RecentWork</div>
+  )
+}
+
+export default RecentWork
