d573d1f8f9e0565811e160ad3906d475c1c8eb10
DibsTHEgreat <pateliyandivya@gmail.com>
2024-05-25 12:45:43 -0600

    hero button fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,91 +1,107 @@
 "use client";
 
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
+import React, { useState, useEffect } from 'react';
+import { Spotlight } from './ui/Spotlight';
 import { TypeAnimation } from "react-type-animation";
 import CoolerButton from './ui/CoolerButton';
 import DownloadCSMobile from './ui/DownloadCSMobile';
 
 const downloadCV = () => {
-    const cvUrl = "/pdf/Divya.P-Resume.pdf";
-  
-    const anchor = document.createElement("a");
-    anchor.href = cvUrl;
-    anchor.target = "_blank";
-    anchor.download = "Divya.P-Resume.pdf"; 
-  
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
-  };
+  const cvUrl = "/pdf/Divya.P-Resume.pdf";
+
+  const anchor = document.createElement("a");
+  anchor.href = cvUrl;
+  anchor.target = "_blank";
+  anchor.download = "Divya.P-Resume.pdf"; 
+
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+};
 
 const Hero = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsLargeScreen(window.innerWidth > 1100);
+    };
+
+    // Check the screen size initially
+    checkScreenSize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', checkScreenSize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
   return (
     <section className="pb-20 pt-36">
-        <div>
-            <Spotlight className="-top-40 -left-10  md:-left-32 md:-top-20 h-screen" fill="white"/>
-            <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
-            <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
-        </div>
+      <div>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
 
-        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-        </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
 
-        <div className="flex flex-col justify-center items-center relative lg:place-items-center pt-28">
-            <h1 className="text-blue-100 max-w-2xl md:text-6xl text-3xl font-extrabold">
-            <span className="text-4xl md:text-6xl">
-                Hello, I&apos;m{" "} A
-            </span>{" "}
-            </h1>
-            <h1 className="text-purple max-w-2xl mb-4 md:text-6xl text-3xl font-extrabold pb-11">
-                <TypeAnimation
-                    sequence={[
-                    "Software Developer",
-                    1000,
-                    "Tech Enthusiast",
-                    1000,
-                    "Problem-Solver",
-                    1000,
-                    "Team-Player",
-                    1000,        
-                    "Student",
-                    1000,      
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    repeat={Infinity}
-                />
-            </h1>
-        </div>
+      <div className="flex flex-col justify-center items-center relative lg:place-items-center pt-28">
+        <h1 className="text-blue-100 max-w-2xl md:text-6xl text-3xl font-extrabold">
+          <span className="text-4xl md:text-6xl">
+            Hello, I&apos;m{" "} A
+          </span>{" "}
+        </h1>
+        <h1 className="text-purple max-w-2xl mb-4 md:text-6xl text-3xl font-extrabold pb-11">
+          <TypeAnimation
+            sequence={[
+              "Software Developer",
+              1000,
+              "Tech Enthusiast",
+              1000,
+              "Problem-Solver",
+              1000,
+              "Team-Player",
+              1000,        
+              "Student",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </h1>
+      </div>
 
-        <div className="flex justify-center items-center relative my-10">
-            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-              <h2 className="uppercase tracking-widest textl-lg lg:text-xl text-center text-blue-100">
-                Hello, welcome to my portfolio! I&apos;m a full-time student at the University of Lethbridge studying
-                Computer Science and Business Management with a minor in Mathematics.
-              </h2>  
-            </div>
+      <div className="flex justify-center items-center relative my-10">
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className="uppercase tracking-widest textl-lg lg:text-xl text-center text-blue-100">
+            Hello, welcome to my portfolio! I&apos;m a full-time student at the University of Lethbridge studying
+            Computer Science and Business Management with a minor in Mathematics.
+          </h2>  
         </div>
+      </div>
 
-        <div className="flex justify-center items-center relative">
-          <div className="hidden lg:block">
-            <CoolerButton 
-              title="Download CV"
-              handleClick={downloadCV}
-            />
-          </div>
-
-          <div className="block lg:hidden">
-            <DownloadCSMobile
-              title="Download CV"
-              handleClick={downloadCV}
-            />
-          </div>
-        </div>
-        
+      <div className="flex justify-center items-center relative">
+        {isLargeScreen ? (
+          <CoolerButton 
+            title="Download CV"
+            handleClick={downloadCV}
+          />
+        ) : (
+          <DownloadCSMobile
+            title="Download CV"
+            handleClick={downloadCV}
+          />
+        )}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
